cda1d4c757f8101f06f5657ca5a369f48020a08b
sudhanshusingh-g <sudsingh32@gmail.com>
2025-02-09 23:42:04 +0530

    improved transition animation

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -13,13 +13,13 @@ function Layout({children}:LayoutProps) {
     <div className="max-w-3/4 mx-auto h-screen">
       {/* Navigation */}
       <nav className="flex items-center justify-between py-4 border-b-[1px] h-[8%]">
-        <ToggleThemeButton/>
+        <ToggleThemeButton />
         <ul className="flex items-center gap-4 font-semibold">
-          <li className="cursor-pointer relative after:absolute after:left-0 after:bottom-0 after:h-[2px] after:bg-black after:w-0 after:transition-all hover:after:w-full">
+          <li className="cursor-pointer relative after:absolute after:left-0 after:bottom-0 after:h-[2px] after:bg-black dark:after:bg-white after:w-0 after:transition-all hover:after:w-full">
             <Link to={"/"}>Blogs</Link>
           </li>
 
-          <li className="cursor-pointer relative after:absolute after:left-0 after:bottom-0 after:h-[2px] after:bg-black after:w-0 after:transition-all hover:after:w-full">
+          <li className="cursor-pointer relative after:absolute after:left-0 after:bottom-0 after:h-[2px] after:bg-black dark:after:bg-white after:w-0 after:transition-all hover:after:w-full">
             <Link to={"/about"}>About</Link>
           </li>
           <li
@@ -33,7 +33,9 @@ function Layout({children}:LayoutProps) {
         </ul>
       </nav>
       {/* Content */}
-      <section className="h-[92%] overflow-scroll no-scrollbar">{children}</section>
+      <section className="h-[92%] overflow-scroll no-scrollbar">
+        {children}
+      </section>
     </div>
   );
 }
--- client/src/components/ToggleThemeButton.tsx
+++ client/src/components/ToggleThemeButton.tsx
@@ -7,7 +7,7 @@ function ToggleThemeButton() {
     <button
       onClick={toggleTheme}
       aria-label={`Switch to ${theme === "dark" ? "light" : "dark"} theme`}
-      className="p-2 rounded bg-gray-200 dark:bg-gray-800 hover:bg-gray-300 dark:hover:bg-gray-700 cursor-pointer"
+      className="p-2 rounded transition-all duration-300 bg-gray-200 dark:bg-gray-800 hover:bg-gray-300 dark:hover:bg-gray-700 cursor-pointer"
     >
       {theme === "dark" ? <MoonStar /> : <Sun />}
     </button>
