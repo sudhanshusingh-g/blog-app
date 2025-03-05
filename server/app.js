c574d1b7d2961b33be4a5801da6db612bdc98b4a
Sudhanshu Singh <36367047+sudhanshusingh-g@users.noreply.github.com>
2025-03-05 22:45:09 +0530

    Update origin url

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://blog-app-uutv.onrender.com",
     credentials:true
   })
 );
